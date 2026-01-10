0b66320580943fdea8a205396667b8928befc17c
sudodocs <ssugandh@sudodocs.com>
2026-01-10 20:47:50 +0530

    Correction

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,6 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
       description="Automated Documentation for Technical Teams">
       <HomepageHeader />
       <main>
